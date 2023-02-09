69beb22b2903d582d447c9271477fd090a12cd83
Maksym Bykovskij <maksbykovskij@gmail.com>
2023-02-09 03:26:21 +0300

    feat: add adaptive

--- js/main.js
+++ js/main.js
@@ -15,10 +15,25 @@ $(document).ready(function () {
         slidesToScroll: 1,
         prevArrow: '<button class="btn slider-btn slider-prev"></button>',
         nextArrow: '<button class="btn slider-btn slider-next active"></button>',
+        responsive: [
+          {
+            breakpoint: 981,
+            settings: {
+              arrows: false,
+              dots: true
+            }
+          },
+        ]
       });
 
       $('.slider-btn').click(function (){
         $('.slider-btn').removeClass('active');
         $(this).addClass('active');
       });
+
+      $(".burger").click(function (){
+        $(".burger").toggleClass("active")
+        $(".nav").toggleClass("active")
+        $("body").toggleClass("hidden")
+      })
 });
